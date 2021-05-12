b2008628a3bde26506649652b1e8c958da9284ec
Ana Paula Pianezer Tank <ana.tank@workdb.com.br>
2021-05-12 18:05:21 -0300

    Criação estrutura inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EditTodoDialogComponent } from './edit-todo-dialog/edit-todo-dialog.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
+import { TodosComponent } from './todos/todos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditTodoDialogComponent,
+    TodoItemComponent,
+    TodosComponent
   ],
   imports: [
     BrowserModule
